0d7b46dee1c062e4c2c43843fc7e600cfe246325
ngocthang2021 <ngnngthang@gmail.com>
2022-11-14 23:05:40 +0700

    Update Resume Page

--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 
+import SkillsList from '../../components/Skills/Skills';
+
+import './Resume.scss';
+
 function Resume() {
 	return (
-		<section
-			className='page-full section resume'
-			id='resume'
-		>
-			Resume
+		<section className='resume'>
+			<div className='resume__header'>
+				<h2 className='resume__title mb-2'>Portfolio</h2>
+				<p className='resume__text'>Here is what I've archived in my learning process.</p>
+			</div>
+
+			<SkillsList />
 		</section>
 	);
 }
