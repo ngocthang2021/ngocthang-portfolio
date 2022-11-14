69635cebec60bd20c7a98d30782dae1a53b36d98
ngocthang2021 <ngnngthang@gmail.com>
2022-11-14 17:42:06 +0700

    Update Skills data

---
+++ src/config/skills.js
@@ -0,0 +1,28 @@
+import htmlIcon from '../assets/svg-icons/html-5-logo.svg';
+import cssIcon from '../assets/svg-icons/css-3-logo.svg';
+import sassIcon from '../assets/svg-icons/sass-svg.svg';
+import javascriptIcon from '../assets/svg-icons/js-svg.svg';
+import reactIcon from '../assets/svg-icons/react-svg.svg';
+import tailwindcssIcon from '../assets/svg-icons/tailwindcss-icon-svg.svg';
+import bootstrapIcon from '../assets/svg-icons/bootstrap-svg.svg';
+import figmaIcon from '../assets/svg-icons/figma-svg.svg';
+import photoshopIcon from '../assets/svg-icons/photoshop-svg.svg';
+
+export const skillsTech = [
+	{ skill: 'HTML', icon: htmlIcon },
+	{ skill: 'CSS', icon: cssIcon },
+	{ skill: 'Javascript', icon: javascriptIcon },
+	{ skill: 'Sass', icon: sassIcon },
+	{ skill: 'Tailwindcss', icon: tailwindcssIcon },
+	{ skill: 'Bootstrap', icon: bootstrapIcon },
+	{ skill: 'React', icon: reactIcon },
+	{ skill: 'Photoshop', icon: photoshopIcon },
+	{ skill: 'Figma', icon: figmaIcon },
+];
+
+export const skillsAbilities = [
+	{
+		skill: '',
+		desc: '',
+	},
+];
