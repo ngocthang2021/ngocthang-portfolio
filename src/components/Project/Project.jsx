479dadb46bd5a98d5859580399359d2bbf263083
ngocthang2021 <ngnngthang@gmail.com>
2022-11-15 18:36:32 +0700

    Udpate project folders

---
+++ src/components/Project/Project.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { projectData } from '../../data/projectData';
+
+import './Project.scss';
+
+function Project() {
+	return <div></div>;
+}
+
+export default Project;
---
+++ src/data/projectData.js
@@ -0,0 +1,23 @@
+export const projectData = [
+	{
+		name: 'Digital Clock',
+		projectImage: '',
+		projectLink: 'https://ngocthang2021.github.io/digital-clock/',
+		projectSource: 'https://github.com/ngocthang2021/digital-clock',
+		projectTech: ['HTML', 'CSS', 'Javascript'],
+	},
+	{
+		name: 'React Movie App',
+		projectImage: '',
+		projectLink: 'https://react-movie-33si.vercel.app/',
+		projectSource: 'https://github.com/ngocthang2021/react-movie-app',
+		projectTech: ['ReactJS', 'CSS (SCSS)', 'Javascript'],
+	},
+	{
+		name: 'Portfolio | Personal Website',
+		projectImage: '',
+		projectLink: 'https://ngocthang-portfolio.vercel.app/',
+		projectSource: 'https://github.com/ngocthang2021/ngocthang-portfolio',
+		projectTech: ['ReactJS', 'CSS (SCSS)', 'Javascript'],
+	},
+];
