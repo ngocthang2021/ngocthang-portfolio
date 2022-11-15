9e19bc3362c7132301c604b8a01285fd6fc9024f
ngocthang2021 <ngnngthang@gmail.com>
2022-11-15 18:43:19 +0700

    Add Project Data

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -1,21 +1,25 @@
+import portfolioImage from '../assets/project-images/ngocthang-portfolio.png';
+import digitalClockImage from '../assets/project-images/digital-clock.png';
+import reactMovieAppImage from '../assets/project-images/react-movie-app.png';
+
 export const projectData = [
 	{
 		name: 'Digital Clock',
-		projectImage: '',
+		projectImage: digitalClockImage,
 		projectLink: 'https://ngocthang2021.github.io/digital-clock/',
 		projectSource: 'https://github.com/ngocthang2021/digital-clock',
 		projectTech: ['HTML', 'CSS', 'Javascript'],
 	},
 	{
 		name: 'React Movie App',
-		projectImage: '',
+		projectImage: reactMovieAppImage,
 		projectLink: 'https://react-movie-33si.vercel.app/',
 		projectSource: 'https://github.com/ngocthang2021/react-movie-app',
 		projectTech: ['ReactJS', 'CSS (SCSS)', 'Javascript'],
 	},
 	{
 		name: 'Portfolio | Personal Website',
-		projectImage: '',
+		projectImage: portfolioImage,
 		projectLink: 'https://ngocthang-portfolio.vercel.app/',
 		projectSource: 'https://github.com/ngocthang2021/ngocthang-portfolio',
 		projectTech: ['ReactJS', 'CSS (SCSS)', 'Javascript'],
