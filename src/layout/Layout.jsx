799b141a54255a7ce786615971197238db09fc95
ngocthang2021 <ngnngthang@gmail.com>
2022-11-15 17:29:30 +0700

    Update Layout with Scroll Top Button

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import Navbar from '../components/Navbar/Navbar';
+import ScrollTopButton from '../components/ScrollTopButton/ScrollTopButton';
 
 function Layout({ children }) {
 	return (
 		<div className='wrapper'>
 			<Navbar />
 			<div className='page-full'>{children}</div>
+			<ScrollTopButton />
 		</div>
 	);
 }
