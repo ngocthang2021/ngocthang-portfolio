f553a3171c79aacd2626af74b148ed2935b22780
ngocthang2021 <ngnngthang@gmail.com>
2022-11-16 17:43:11 +0700

    Update Profile Data

--- src/data/profileData.js
+++ src/data/profileData.js
@@ -1,8 +1,26 @@
-import { FaFacebook, FaGithub, FaLinkedin, FaInstagram } from 'react-icons/fa';
+import {
+	FaFacebook,
+	FaGithub,
+	FaLinkedin,
+	FaInstagram,
+	FaGoogle,
+	FaPhoneAlt,
+	FaUser,
+	FaRegCalendarAlt,
+	FaMapMarkerAlt,
+} from 'react-icons/fa';
 
-export const profile = [
+export const profileSocial = [
 	{ social: 'Facebook', path: 'https://www.facebook.com/nnt.2011/', icon: FaFacebook },
 	{ social: 'Instagram', path: 'https://www.instagram.com/nnt.20/', icon: FaInstagram },
 	{ social: 'Github', path: 'https://github.com/ngocthang2021', icon: FaGithub },
 	{ social: 'LinkedIn', path: 'https://www.linkedin.com/in/ngngthang/', icon: FaLinkedin },
+];
+
+export const profileInformation = [
+	{ profileTag: 'Name', profileInfo: 'Nguyễn Ngọc Thắng', icon: FaUser },
+	{ profileTag: 'DoB', profileInfo: '20/11/1999', icon: FaRegCalendarAlt },
+	{ profileTag: 'Phone', profileInfo: '+84986834992', icon: FaPhoneAlt },
+	{ profileTag: 'Address', profileInfo: '18, Street 13, Long Binh Ward, Thu Duc City, HCMC', icon: FaMapMarkerAlt },
+	{ profileTag: 'Email', profileInfo: 'ng.ngocthang2021@gmail.com', icon: FaGoogle },
 ];
