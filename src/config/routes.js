894c6edc679ad294114cb5666a12a09f11682cb8
ngocthang2021 <ngnngthang@gmail.com>
2022-11-17 01:58:23 +0700

    Remove Contact Page

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,11 +1,9 @@
 import Home from '../pages/Home/Home';
 import About from '../pages/About/About';
 import Resume from '../pages/Resume/Resume';
-import Contact from '../pages/Contact/Contact';
 
 export const pageRoutes = [
 	{ path: '/', element: Home },
 	{ path: '/about', element: About },
 	{ path: '/resume', element: Resume },
-	{ path: '/contact', element: Contact },
 ];
