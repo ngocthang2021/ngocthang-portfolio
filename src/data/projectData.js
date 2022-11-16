1cc80a91e9c52b6afaf9ebfce436b25862d26cad
ngocthang2021 <ngnngthang@gmail.com>
2022-11-16 15:18:01 +0700

    Update Project Data

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -1,27 +1,43 @@
 import portfolioImage from '../assets/project-images/ngocthang-portfolio.png';
 import digitalClockImage from '../assets/project-images/digital-clock.png';
 import reactMovieAppImage from '../assets/project-images/react-movie-app.png';
+import forkifyAppImage from '../assets/project-images/forkify.png';
 
 export const projectData = [
 	{
-		name: 'Digital Clock',
+		projectTitle: 'Digital Clock',
 		projectImage: digitalClockImage,
+		projectDescription: 'A simple colorful digital clock with 12-hour and 24-hour format.',
+		projectStatus: 'completed',
+		projectTech: ['HTML', 'CSS', 'Javascript'],
 		projectLink: 'https://ngocthang2021.github.io/digital-clock/',
 		projectSource: 'https://github.com/ngocthang2021/digital-clock',
-		projectTech: ['HTML', 'CSS', 'Javascript'],
 	},
 	{
-		name: 'React Movie App',
+		projectTitle: 'React Movie App',
 		projectImage: reactMovieAppImage,
+		projectDescription: 'A simple website for searching information about movies or TV shows.',
+		projectStatus: 'completed',
+		projectTech: ['ReactJS', 'CSS (SCSS)', 'Javascript'],
 		projectLink: 'https://react-movie-33si.vercel.app/',
 		projectSource: 'https://github.com/ngocthang2021/react-movie-app',
-		projectTech: ['ReactJS', 'CSS (SCSS)', 'Javascript'],
 	},
 	{
-		name: 'Portfolio | Personal Website',
+		projectTitle: 'Portfolio | Personal Website',
 		projectImage: portfolioImage,
+		projectDescription: 'My portfolio using React.',
+		projectStatus: 'completed',
+		projectTech: ['ReactJS', 'CSS (SCSS)', 'Javascript'],
 		projectLink: 'https://ngocthang-portfolio.vercel.app/',
 		projectSource: 'https://github.com/ngocthang2021/ngocthang-portfolio',
-		projectTech: ['ReactJS', 'CSS (SCSS)', 'Javascript'],
+	},
+	{
+		projectTitle: 'Forkify',
+		projectImage: forkifyAppImage,
+		projectDescription: 'My portfolio using React.',
+		projectStatus: 'ongoing',
+		projectTech: ['HTML', 'CSS (SCSS)', 'Javascript'],
+		projectLink: 'https://ngocthang-portfolio.vercel.app/',
+		projectSource: 'https://github.com/ngocthang2021/forkify-nnt',
 	},
 ];
